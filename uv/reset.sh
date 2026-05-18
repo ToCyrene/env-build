8de23798a25e23fee883192b54b8327de0411386
ToCyrene <2635356280@qq.com>
2026-05-19 01:17:12 +0800

    uv

---
+++ uv/reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TARGET_DIR="$HOME/.uv_env"
+TARGET_FILE="$TARGET_DIR/.command"
+BASHRC="$HOME/.bashrc"
+
+rm -rf "$TARGET_DIR"
+
+if [ -f "$BASHRC" ]; then
+    sed -i "/if \[ -f \"${TARGET_FILE//\//\\/}\" \]; then/,/fi/d" "$BASHRC"
+    sed -i '${/^[[:space:]]*$/d}' "$BASHRC"
+fi
